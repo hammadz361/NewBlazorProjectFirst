dd7a7463fea52c69ba3b7f945b43625103a7548f
Muhammad Hammad <usamaz361@gmail.com>
2022-03-03 17:24:15 +0500

    first commit

--- NewBlazorProject/Client/Provider/CustomUserFactory.cs
+++ NewBlazorProject/Client/Provider/CustomUserFactory.cs
@@ -8,6 +8,7 @@ namespace NewBlazorProject.Client.Provider
     public class CustomUserFactory
     : AccountClaimsPrincipalFactory<RemoteUserAccount>
     {
+        // git testing
         public CustomUserFactory(IAccessTokenProviderAccessor accessor)
             : base(accessor)
         {
