07ae6edbd47e9b5f73a840a46980f023cf08dd85
hammadz361 <63950024+hammadz361@users.noreply.github.com>
2022-03-03 19:09:57 +0500

    Revert "Update CustomUserFactory.cs"
    This reverts commit 0165e9fc8ed840235d42cf52761a5d9b48e0d49c.

--- NewBlazorProject/Client/Provider/CustomUserFactory.cs
+++ NewBlazorProject/Client/Provider/CustomUserFactory.cs
@@ -16,7 +16,6 @@ namespace NewBlazorProject.Client.Provider
         // commit all
         // haroon and inzamam
         // InziMalik
-        
         public override async ValueTask<ClaimsPrincipal> CreateUserAsync(
             RemoteUserAccount account,
             RemoteAuthenticationUserOptions options)
