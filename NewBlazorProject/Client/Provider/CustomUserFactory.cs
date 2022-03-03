1544752beba4f702bbfc98beb633dfdf768ac682
haroon khan <haroon33510@gmail.com>
2022-03-03 19:06:30 +0500

    From Hammad

--- NewBlazorProject/Client/Provider/CustomUserFactory.cs
+++ NewBlazorProject/Client/Provider/CustomUserFactory.cs
@@ -16,6 +16,7 @@ namespace NewBlazorProject.Client.Provider
         // commit all
         // haroon and inzamam
         // InziMalik
+        //Hammad from Haroom Laptop
         public override async ValueTask<ClaimsPrincipal> CreateUserAsync(
             RemoteUserAccount account,
             RemoteAuthenticationUserOptions options)
