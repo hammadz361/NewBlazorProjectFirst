a25cedb37c484082617e1d35ebf5ace6652e4a81
hammadz361 <63950024+hammadz361@users.noreply.github.com>
2022-03-03 18:56:18 +0500

    Update ApplicationUser.cs
    From Hammad

--- NewBlazorProject/Server/Models/ApplicationUser.cs
+++ NewBlazorProject/Server/Models/ApplicationUser.cs
@@ -4,8 +4,5 @@ namespace NewBlazorProject.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        //inzii
-        //Inzi2
-        // haroomn ka laptop mai masla hai
     }
 }
