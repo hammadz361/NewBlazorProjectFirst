31e3c81a16a2e388d5d37984f1851188e3e69b48
hammadz361 <63950024+hammadz361@users.noreply.github.com>
2022-03-04 19:02:42 +0500

    Interface ready

--- NewBlazorProject/Server/InterFaces/IAdminInterface.cs
+++ NewBlazorProject/Server/InterFaces/IAdminInterface.cs
@@ -2,12 +2,21 @@
 
 namespace NewBlazorProject.Server.InterFaces
 {
+    /*1- Create Methods
+    2- Get All Methods
+    3- Get By Id Methods
+    4- Update Methods
+    5- Delete Methods*/
     public interface IAdminInterface
     {
         MD_Address AddAddress(MD_Address mD_Address);
-        Task<MD_Address> GetAddressById(int Id);
+
         Task<IEnumerable<MD_Address>> GetAllAddress();
+
+        Task<MD_Address> GetAddressById(int Id);
+
         void UpdateAddress(MD_Address mD_Address);
+
         void DeleteAddress(int Id);
     }
 }
