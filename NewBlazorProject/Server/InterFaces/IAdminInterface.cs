a4fc4dba15e5a9bf85b22f19db24cd4d48206e73
hammadz361 <63950024+hammadz361@users.noreply.github.com>
2022-03-04 19:02:42 +0500

    Commit my Saeed

--- NewBlazorProject/Server/InterFaces/IAdminInterface.cs
+++ NewBlazorProject/Server/InterFaces/IAdminInterface.cs
@@ -2,12 +2,38 @@
 
 namespace NewBlazorProject.Server.InterFaces
 {
+    /*1- Create Methods
+    2- Get All Methods
+    3- Get By Id Methods
+    4- Update Methods
+    5- Delete Methods*/
     public interface IAdminInterface
     {
         MD_Address AddAddress(MD_Address mD_Address);
-        Task<MD_Address> GetAddressById(int Id);
+        MD_CreatedBy AddCreatedBy(MD_CreatedBy mD_CreatedBy);
+        MD_Directory AddDirectory(MD_Directory mD_Directory);
+        MD_Education AddEducation(MD_Education mD_Education);
+
         Task<IEnumerable<MD_Address>> GetAllAddress();
+        Task<IEnumerable<MD_CreatedBy>> GetAllCreatedBy();
+        Task<IEnumerable<MD_Directory>> GetAllDirectory();
+        Task<IEnumerable<MD_Education>> GetAllEducation();
+
+
+        Task<MD_Address> GetAddressById(int Id);
+        Task<MD_CreatedBy> GetCreatedById(int Id);
+        Task<MD_Directory> GetDirectoryById(int Id);
+        Task<MD_Education> GetEducationById(int Id);
+
         void UpdateAddress(MD_Address mD_Address);
+        void UpdateCreatedBy(MD_CreatedBy mD_CreatedBy);
+        void UpdateDirectory(MD_Directory mD_Directory);
+        void UpdateEducation(MD_Education mD_Education);
+
         void DeleteAddress(int Id);
+        void DeleteCreatedBy(int Id);
+        void DeleteDirectory(int Id);
+        void DeleteEducation(int Id);
+        
     }
 }
