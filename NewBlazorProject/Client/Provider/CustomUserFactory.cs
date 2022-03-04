aa94e8396aa52572f45f6d72d6631fb08d1942d3
hammadz361 <63950024+hammadz361@users.noreply.github.com>
2022-03-04 15:58:34 +0500

    from hammad

--- NewBlazorProject/Client/Provider/CustomUserFactory.cs
+++ NewBlazorProject/Client/Provider/CustomUserFactory.cs
@@ -8,7 +8,6 @@ namespace NewBlazorProject.Client.Provider
     public class CustomUserFactory
     : AccountClaimsPrincipalFactory<RemoteUserAccount>
     {
-        // form saeed
         public CustomUserFactory(IAccessTokenProviderAccessor accessor)
             : base(accessor)
         {
