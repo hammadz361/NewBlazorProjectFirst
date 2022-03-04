00a67066e57881b79032fb32a58911ac840ab87d
Saeed Ahmed <saeed.cs17@iba-suk.edu.pk>
2022-03-04 18:37:16 +0500

    saeed

--- NewBlazorProject/Server/Data/ApplicationDbContext.cs
+++ NewBlazorProject/Server/Data/ApplicationDbContext.cs
@@ -5,7 +5,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using NewBlazorProject.Server.Models;
 using NewBlazorProject.Shared.Models;
-using NewBlazorProject.Shared.Models.HR_Data;
 using NewBlazorProject.Shared.Models.MasterData;
 
 namespace NewBlazorProject.Server.Data
@@ -33,7 +32,5 @@ namespace NewBlazorProject.Server.Data
         public DbSet<MD_Address> MD_Addresses { get; set; }
         public DbSet<MD_UploadFile> MD_UploadFiles { get; set; }
         public DbSet<MD_Directory> MD_Directories { get; set; }
-        public DbSet<HR_Clearance> HR_Clearances { get; set; }
-        public DbSet<HR_Designation> HR_Designations { get; set; }
     }
 }
