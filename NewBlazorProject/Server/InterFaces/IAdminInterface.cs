bf3331fe93b930289a48b1e830e72ec6204444c7
rahim <rahim@DESKTOP-ESHRNK6>
2022-03-04 19:26:20 +0500

    commit by inzi

--- NewBlazorProject/Server/InterFaces/IAdminInterface.cs
+++ NewBlazorProject/Server/InterFaces/IAdminInterface.cs
@@ -10,13 +10,33 @@ namespace NewBlazorProject.Server.InterFaces
     public interface IAdminInterface
     {
         MD_Address AddAddress(MD_Address mD_Address);
+        MD_UploadFile AddUploadFile(MD_UploadFile mD_UploadFile);
+        MD_UpdatedBy AddUpdatedBy (MD_UpdatedBy mD_UpdatedBy);
+        MD_Gender AddGender(MD_Gender mD_Gender);
+        MD_EducationLevel AddEducationLevel(MD_EducationLevel mD_EducationLevel);
 
         Task<IEnumerable<MD_Address>> GetAllAddress();
+        Task<IEnumerable<MD_UploadFile>> GetAllUploadFiles();
+        Task<IEnumerable<MD_UpdatedBy>> GetAllUpdatedBy();
+        Task<IEnumerable<MD_Gender>> GetAllGender();
+        Task<IEnumerable<MD_EducationLevel>> GetAllEducationLevel();
 
         Task<MD_Address> GetAddressById(int Id);
+        Task<MD_UploadFile> GetUploadFileById(int Id);
+        Task<MD_UpdatedBy> GetUpdatedById(int Id);
+        Task <MD_Gender> GetGenderById (int Id);
+        Task<MD_EducationLevel> GetEducationLevelById(int Id);
 
         void UpdateAddress(MD_Address mD_Address);
+        void UpdateUploadFile(MD_UploadFile mD_UploadFile);
+        void UpdateUpdatedBy (MD_UpdatedBy mD_UpdateUpdatedBy);
+        void UpdateGender(MD_Gender mD_Gender);
+        void UpdateEducationLevel(MD_EducationLevel mD_EducationLevel);
 
         void DeleteAddress(int Id);
+        void DeleteUploadFile(int Id);
+        void DeleteUpdatedBy(int Id);
+        void DeleteGender (int Id);
+        void DeleteEducationLevel(int Id);
     }
 }
