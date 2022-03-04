67d8dcf64ff808fa6935603e17746280af67fb79
hammadz361 <63950024+hammadz361@users.noreply.github.com>
2022-03-04 18:48:25 +0500

    Started Interface Admin

--- NewBlazorProject/Server/InterFaces/IAdminInterface.cs
+++ NewBlazorProject/Server/InterFaces/IAdminInterface.cs
@@ -1,6 +1,13 @@
-﻿namespace NewBlazorProject.Server.InterFaces
+﻿using NewBlazorProject.Shared.Models.MasterData;
+
+namespace NewBlazorProject.Server.InterFaces
 {
     public interface IAdminInterface
     {
+        MD_Address AddAddress(MD_Address mD_Address);
+        Task<MD_Address> GetAddressById(int Id);
+        Task<IEnumerable<MD_Address>> GetAllAddress();
+        void UpdateAddress(MD_Address mD_Address);
+        void DeleteAddress(int Id);
     }
 }
